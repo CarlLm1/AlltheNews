1139e211a5f284f9e65e8e88b379416d2dd05f91
Carl Medina <carl.l.medina@comcast.net>
2019-10-17 16:00:33 -0700

    added more packages and dependencies

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// Dependencies
+var body = require("body-parser");
 var express = require("express");
+var exphdbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var req = require("request");
 
 // scraping tools
 
