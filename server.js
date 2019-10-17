01bd86488c8ae7225c6c4c61ee2cfd7f97267315
Carl Medina <carl.l.medina@comcast.net>
2019-10-17 16:50:08 -0700

    setting up/modifying server.js

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 // Dependencies
-var body = require("body-parser");
 var express = require("express");
-var exphdbs = require("express-handlebars");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var req = require("request");
 
-// scraping tools
+// Setup host to be on designated port 3000
+var PORT = process.env.PORT || 3000;
 
-var axios = require("axios");
-var cheerio = require("cheerio");
+var app = express();
 
-// Require all models
-var db = require("./models");
+// Setup Express Router
+var router = express.Router();
+
+// The public folder will be the static directory
+app.use(express.static(__dirname + "/public"));
+
+// Have requests go thru the router middleware
+app.use(router);
+
+// Listen on the port
+app.listen(PORT, function() {
+  console.log("Listening on port:" + PORT);
+});
+
+// var body = require("body-parser");
+
+// var exphdbs = require("express-handlebars");
+// var logger = require("morgan");
+// var mongoose = require("mongoose");
+// var req = require("request");
+
+// // scraping tools
+
+// var axios = require("axios");
+// var cheerio = require("cheerio");
+
+// // Require all models
+// var db = require("./models");
