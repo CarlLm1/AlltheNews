e0dbcea4f33241d01127c4aee440de561b54a5d4
Carl Medina <carl.l.medina@comcast.net>
2019-10-17 17:04:33 -0700

    setup mongoose connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+var exphdbs = require("express-handlebars");
+var mongoose = require("mongoose");
 
 // Setup host to be on designated port 3000
 var PORT = process.env.PORT || 3000;
@@ -12,19 +15,45 @@ var router = express.Router();
 // The public folder will be the static directory
 app.use(express.static(__dirname + "/public"));
 
+// Handlebars
+app.engine(
+  "handlebars",
+  exphdbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
+//bodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
 // Have requests go thru the router middleware
 app.use(router);
+
+// using the deployed database.
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoAlltheNews";
+
+// Connect mongoose to the database
+mongoose.connect(db, function(error) {
+  // Log any errors or log a success message
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Mongoose connection successful!");
+  }
+});
 
 // Listen on the port
 app.listen(PORT, function() {
   console.log("Listening on port:" + PORT);
 });
 
-// var body = require("body-parser");
-
-// var exphdbs = require("express-handlebars");
 // var logger = require("morgan");
-// var mongoose = require("mongoose");
+
 // var req = require("request");
 
 // // scraping tools
