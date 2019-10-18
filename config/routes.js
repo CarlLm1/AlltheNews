bf005ac4cd8bd616e1d2689f638c168734fc27b4
Carl Medina <carl.l.medina@comcast.net>
2019-10-17 18:19:50 -0700

    added css style

---
+++ config/routes.js
@@ -0,0 +1,10 @@
+module.exports = function(router) {
+  // Route for rendering the homepage
+  router.get("/", function(req, res) {
+    res.render("home");
+  });
+  // Route for rendering the Saved page
+  router.get("/saved", function(req, res) {
+    res.render("saved");
+  });
+};
